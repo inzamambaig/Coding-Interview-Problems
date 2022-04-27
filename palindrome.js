1d881aa46344b92161767e0b9889b539b3eb6883
Inzamam Baig <baigsone@gmail.com>
2022-04-27 19:24:18 +0500

    New Solutions Steps

--- palindrome.js
+++ palindrome.js
@@ -3,36 +3,36 @@ Solution 1.1
 White Spaces, punctuations and special characters are not included
 */
 
-// function palindrome(str) {
-//   const removedSpecialChar = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
-//   const reversedStr = removedSpecialChar.split("").reverse().join("");
-//   return removedSpecialChar === reversedStr;
-// }
+function palindrome(str) {
+  const removedSpecialChar = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const reversedStr = removedSpecialChar.split("").reverse().join("");
+  return removedSpecialChar === reversedStr;
+}
 
 /* 
 Solution 1.2
 White Spaces, punctuations and special characters are included
 */
 
-// function palindrome(str) {
-//   const reversedStr = str.split("").reverse().join("");
-//   return reversedStr === str;
-// }
-// console.log(palindrome("abba"));
+function palindrome(str) {
+  const reversedStr = str.split("").reverse().join("");
+  return reversedStr === str;
+}
+console.log(palindrome("abba"));
 
 /* 
 Solution 2.1
 White Spaces, punctuations and special characters are included
 */
 
-// function palindrome(str) {
-//   for (let i = 0; i < str.length / 2; i++) {
-//     if (str[i] !== str[str.length - 1 - i]) {
-//       return false;
-//     }
-//   }
+function palindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
 
-//   return true;
+  return true;
 // }
 
 // console.log(palindrome("abba"));
@@ -42,17 +42,17 @@ Solution 2.2
 White Spaces, punctuations and special characters are not included
 */
 
-// function palindrome(str) {
-//   const re = /[^A-Za-z0-9]/g;
-//   str = str.toLowerCase().replace(re, ""); // Remove
+function palindrome(str) {
+  const re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, ""); // Remove
 
-//   for (let i = 0; i < str.length / 2; i++) {
-//     if (str[i] !== str[str.length - 1 - i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 // console.log(palindrome("A man, a plan, a canal. Panama"));
 
--- steps.js
+++ steps.js
@@ -25,4 +25,27 @@ function steps(n) {
   }
 }
 
+/* 
+Solution 3
+Recursion
+*/
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  steps(n, row, stair);
+}
+
 steps(4);
